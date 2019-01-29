08e93b3af58f864680a86dc301017d0c372c2414
saarakhk <saara.vallik@khk.ee>
2019-01-29 12:09:42 +0200

    menu - lisatud index.php failile sisu

--- menu/index.php
+++ menu/index.php
@@ -1,7 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Saara
- * Date: 29.01.2019
- * Time: 11:32
- */
+require_once 'lib/fnk.php';
+require_once 'menus/menu.php';
+// lehe sisu
+// päis - header ja muud vajalikud osad
+pais();
+// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
+menu(array(
+    'praed' => $praed,
+    'supid' => $supid,
+    'magus' => $magusad,
+    'joogid' => $joogid
+));
+// jalus - kõik JS asjad
+jalus();
+?>
