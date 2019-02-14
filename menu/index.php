c788cfe69cfb3aa49732971834c736e022e7cdc0
saarakhk <saara.vallik@khk.ee>
2019-02-14 11:18:54 +0200

    oop menu - loodud pea mall (peatemplate) test

--- menu/index.php
+++ menu/index.php
@@ -1,16 +1,32 @@
 <?php
-require_once 'lib/fnk.php';
-require_once 'menus/menu.php';
+//  require_once 'lib/fnk.php';
+//  require_once 'menus/menu.php';
 // lehe sisu
 // päis - header ja muud vajalikud osad
-pais();
+//  pais();
 // menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
-menu(array(
-    'praed' => $praed,
-    'supid' => $supid,
-    'magus' => $magusad,
-    'joogid' => $joogid
-));
+//  menu(array(
+   // 'praed' => $praed,
+   // 'supid' => $supid,
+    //  'magus' => $magusad,
+   //   'joogid' => $joogid
+//  ));
 // jalus - kõik JS asjad
-jalus();
+//jalus();
+
+
+require_once 'conf.php';
+
+$mainTmpl = new Template ('main');
+
+$mainTmpl->set('title', 'Menu App');
+$mainTmpl->set ('content', 'Menu App Content');
+
+
+echo $mainTmpl->parse();
+
+//  echo '<pre>';
+//  print_r($mainTmpl);
+//  echo '</pre>';
+
 ?>
