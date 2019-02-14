f27cffbb28fd1972f7cd91a8ee5af74b301a3d58
saarakhk <saara.vallik@khk.ee>
2019-02-14 13:03:38 +0200

    oop menu - parandatud menüü kokku surumine kategooia andmete abil

--- menu/index.php
+++ menu/index.php
@@ -1,48 +1,61 @@
 <?php
-//  require_once 'lib/fnk.php';
-//  require_once 'menus/menu.php';
-// lehe sisu
-// päis - header ja muud vajalikud osad
-//  pais();
-// menüü elemendid - menu funktsioon kasutab menu_element funktsiooni sisu loomiseks
-//  menu(array(
-   // 'praed' => $praed,
-   // 'supid' => $supid,
-    //  'magus' => $magusad,
-   //   'joogid' => $joogid
-//  ));
-// jalus - kõik JS asjad
-//jalus();
-
-
 require_once 'conf.php';
 // loome vajalikud vaade objektid
-$mainTmpl = new Template ('main');
+$mainTmpl = new Template('main');
 $contentTmpl = new Template('content');
-$cardTmpl = new Template('card');
-$cardHeaderTmpl = new Template('header');
-$cardDataTmpl = new Template('data');
-$listTmpl = new Template('list');
-
+$categories = array(
+    array(
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'name' => 'Sealihapada ploomide ja aprikoosiga',
+                'description' => 'sealihapada, lisand, salat, leib',
+                'price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'name' => 'Praetud kanakints',
+                'description' => 'praetud kana, lisand, salat, leib',
+                'price' => 2.50,
+                'discount' => 2.13
+            )
+        )
+    ),
+    array(
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'name' => 'Rassolnik',
+                'description' => 'supp, hapukoor, leib',
+                'price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
+);
 $mainTmpl->set('title', 'Menu App');
-
-$cardDataTmpl->add('dish_list', $listTmpl->parse());
-
-$cardTmpl-> set('card_header', $cardHeaderTmpl->parse());
-$cardTmpl-> set('card_data', $cardDataTmpl->parse());
-
-$contentTmpl->add ('cards', $cardTmpl->parse());
-$contentTmpl->add ('cards', $cardTmpl->parse());
-$contentTmpl->add ('cards', $cardTmpl->parse());
-$contentTmpl->add ('cards', $cardTmpl->parse());
-
-$mainTmpl->set ('content', $contentTmpl->parse());
-
-
+foreach ($categories as $category){
+    $cardTmpl = new Template('card');
+    $cardHeaderTmpl = new Template('header');
+    $cardDataTmpl = new Template('data');
+    $cardHeaderTmpl->set('category', $category['name']);
+    $cardHeaderTmpl->set('icon', $category['icon']);
+    $cardTmpl->set('card_header', $cardHeaderTmpl->parse());
+    $cardDataTmpl->set('category', $category['name']);
+    $listTmpl = new Template('list');
+    foreach ($category['data'] as $dish){
+        foreach ($dish as $name=>$value){
+            $listTmpl->set($name, $value);
+        }
+        $cardDataTmpl->add('dish_list', $listTmpl->parse());
+    }
+    $cardTmpl->set('card_data', $cardDataTmpl->parse());
+    $contentTmpl->add('cards', $cardTmpl->parse());
+}
+$mainTmpl->set('content', $contentTmpl->parse());
 echo $mainTmpl->parse();
-
-//  echo '<pre>';
-//  print_r($mainTmpl);
-//  echo '</pre>';
-
-?>
+//echo '<pre>';
+//print_r($mainTmpl);
+//echo '</pre>';
