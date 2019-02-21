59b1780225443d6ebf82f311b5add0247244fa70
saarakhk <saara.vallik@khk.ee>
2019-02-21 10:38:05 +0200

    oop menu - lehe loomine üle viidud

--- menu/menus/menu.php
+++ menu/menus/menu.php
@@ -1,58 +1,34 @@
 <?php
-$praed = array(
+$categories = array(
     array(
-        'nimetus' => 'Sealihapada ploomide ja aprikoosiga',
-        'kirjeldus' => 'sealihapada, lisand, salat, leib',
-        'hind' => 2.65),
+        'name' => 'praed',
+        'icon' => 'fa-utensils',
+        'data' => array(
+            array(
+                'name' => 'Sealihapada ploomide ja aprikoosiga',
+                'description' => 'sealihapada, lisand, salat, leib',
+                'price' => 2.65,
+                'discount' => 2.25
+            ),
+            array(
+                'name' => 'Praetud kanakints',
+                'description' => 'praetud kana, lisand, salat, leib',
+                'price' => 2.50,
+                'discount' => 2.13
+            )
+        )
+    ),
     array(
-        'nimetus' => 'Praetud kanakints',
-        'kirjeldus' => 'praetud kana, lisand, salat, leib',
-        'hind' => 2.50),
-    array(
-        'nimetus' => 'Hakklihakaste',
-        'kirjeldus' => 'hakklihakaste, lisand, salat, leib',
-        'hind' => 2.45),
-    array(
-        'nimetus' => 'Kartul, kaste, salat, leib',
-        'kirjeldus' => '',
-        'hind' => 1.38),
-    array(
-        'nimetus' => 'Hakklihakaste 1/2',
-        'kirjeldus' => 'hakklihakaste, lisand, salat, leib',
-        'hind' => 1.30)
-);
-$supid = array(
-    array(
-        'nimetus' => 'Rassolnik',
-        'kirjeldus' => 'supp, hapukoor, leib',
-        'hind' => 1.10),
-    array(
-        'nimetus' => 'Borss',
-        'kirjeldus' => 'supp, hapukoor, leib',
-        'hind' => 1.30)
-);
-$magusad = array(
-    array(
-        'nimetus' => 'Rosinakisell vahukoorega',
-        'kirjeldus' => '',
-        'hind' => 1.05),
-);
-$joogid = array(
-    array(
-        'nimetus' => 'Mahl',
-        'kirjeldus' => '',
-        'hind' => 0.60),
-    array(
-        'nimetus' => 'Morss',
-        'kirjeldus' => '',
-        'hind' => 0.25),
-    array(
-        'nimetus' => 'Piim',
-        'kirjeldus' => '',
-        'hind' => 0.30),
-    array(
-        'nimetus' => 'Keefir',
-        'kirjeldus' => '',
-        'hind' => 0.39)
+        'name' => 'supid',
+        'icon' => 'fa-utensil-spoon',
+        'data' => array(
+            array(
+                'name' => 'Rassolnik',
+                'description' => 'supp, hapukoor, leib',
+                'price' => 1.10,
+                'discount' => 0.94
+            )
+        )
+    )
 );
 ?>
